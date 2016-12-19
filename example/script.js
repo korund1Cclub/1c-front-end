4dd05ba44173ee4ffa4ec770783090aa239f3762
korundClub <mihailtitof@yandex.ru>
2016-12-19 11:22:56 +0300

    Часы начало. Добавлены деления.

--- example/script.js
+++ example/script.js
@@ -1,14 +1,22 @@
 var canvas = document.getElementById("sandbox"),
 context = canvas.getContext("2d"),
-line = new Path2D();
-line.moveTo(0,0);
-line.lineTo(300, 300);
+clock = new Path2D();
+clock.arc(150, 150, 150, 0, 2 * Math.PI);
+context.stroke(clock);
 
-context.lineWidth = 5;
-context.stroke(line);
+var R = 300/2;
+var d, angle, pX, pY, qX, qY;
+for(d = 0; d < 60; ++d){
+    angle = (d/60) * (2*Math.PI);
+    pX = Math.cos(angle) * R;
+    pY = -Math.sin(angle) * R;
+    qX = 0.9 * pX;
+    qY = 0.9 * pY;
+    pX += R; pY += R;
+    qX += R; qY += R;
 
-context.fillStyle = "red";
-context.fillRect(0,0,200,200);
-
-context.fillStyle = "rgba(0, 255, 0, 0.5)";
-context.fillRect(100, 100, 300, 300);
+    var del = new Path2D();
+    del.moveTo(pX, pY);
+    del.lineTo(qX, qY);
+    context.stroke(del);
+}
