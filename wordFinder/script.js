d044ab61690a2735c792a18f05cf66f5a807a819
korundClub <mihailtitof@yandex.ru>
2016-12-26 17:37:31 +0300

    Поиск слов

---
+++ wordFinder/script.js
@@ -0,0 +1,34 @@
+var textarea = document.getElementById("sandbox"),
+    button = document.getElementById("find");
+
+button.addEventListener("click", function() {
+    var text, words, dictionary = {};
+    /* предполагается, что школьники решат задачу серией вызовов replace для лишних символов    */
+    text = textarea.value.replace(/[^А-Яа-яA-Za-z]/g, " ");
+    words = text.split(" ").filter(function(word)  {
+        return word.length > 4;
+    }).map(function(word) {
+    return word.toLowerCase();
+
+});
+words.forEach(function(word) {
+    if (!(word in dictionary)) {
+        dictionary[word] = 0;
+    }
+    dictionary[word] += 1;
+
+});
+
+
+var maxWord, maxCount = 0, word;
+for (word in dictionary) {
+    if (dictionary[word] > maxCount) {
+        maxCount = dictionary[word];
+        maxWord = word;
+
+    }
+}
+
+
+alert(maxWord);
+});
