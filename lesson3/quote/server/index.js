15d7af11fea29ca56f50e76b6fa8dbdaba256f4a
korundClub <mihailtitof@yandex.ru>
2017-01-09 13:41:15 +0300

    Цитатки

---
+++ lesson3/quote/server/index.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+	cors = require("cors"),
+	fs = require("fs");
+
+var app = express();
+app.use(cors());
+
+app.get("/quote", function(req, res) {
+	fs.readFile("database.txt", "utf-8", function(err, content) {
+		var quotes = content.split("\n");
+		var index = Math.floor(Math.random() * (quotes.length - 1));
+		var result = {
+			number: index + 1,
+			text: quotes[index]
+		};
+		res.send(JSON.stringify(result));
+	});
+	
+});
+
+app.listen(591);
